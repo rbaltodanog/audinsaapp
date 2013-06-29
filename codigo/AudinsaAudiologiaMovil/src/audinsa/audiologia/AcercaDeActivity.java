87c9e7afca6488e6f46326db979b24098f32fd2d
rbaltodanog <rbaltodanog@gmail.com>
2013-06-29 17:38:26 +0000

    Audinsa audiologia - Pantalla Acerca De

---
+++ codigo/AudinsaAudiologiaMovil/src/audinsa/audiologia/AcercaDeActivity.java
@@ -0,0 +1,25 @@
+package audinsa.audiologia;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.Window;
+
+public class AcercaDeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_acerca_de);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_acerca_de, menu);
+		return true;
+	}
+
+}
