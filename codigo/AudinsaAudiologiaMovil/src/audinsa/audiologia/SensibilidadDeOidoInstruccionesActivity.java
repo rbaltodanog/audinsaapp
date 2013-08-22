6307b48bed75cf15a130bea38b68af31b5a20a9a
daniulate <daniulate@gmail.com>
2013-08-22 03:45:17 +0000

    pantalla instrucciones sensibilidad de o'ido

---
+++ codigo/AudinsaAudiologiaMovil/src/audinsa/audiologia/SensibilidadDeOidoInstruccionesActivity.java
@@ -0,0 +1,38 @@
+package audinsa.audiologia;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class SensibilidadDeOidoInstruccionesActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sensibilidad_de_oido_instrucciones);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(
+				R.menu.activity_sensibilidad_de_oido_instrucciones, menu);
+		return true;
+	}
+	public void onCancelarClick(View view) {
+		this.finish();
+	}
+
+	public void onEmpezarClick(View view) {
+		
+		long perfil=getIntent().getLongExtra("idPerfil",0);
+		long tipoExamen=getIntent().getLongExtra("idTipoExamen",0);
+				
+		Intent intent = new Intent(view.getContext(), CuestionarioExamenActivity.class);
+		intent.putExtra("idPerfil", perfil);
+		intent.putExtra("idTipoExamen", tipoExamen);
+		startActivity(intent);
+	}
+}
