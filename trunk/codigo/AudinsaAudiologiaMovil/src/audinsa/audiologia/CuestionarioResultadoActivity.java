3be3e104f264bdb39b418c5c8a48fc24bc347219
daniulate@gmail.com <daniulate@gmail.com@3e832744-ce6b-47cc-aa80-82fe731f6e6d>
2013-08-26 02:57:10 +0000

    ajustes posteriores a la reunión agosto

--- trunk/codigo/AudinsaAudiologiaMovil/src/audinsa/audiologia/CuestionarioResultadoActivity.java
+++ trunk/codigo/AudinsaAudiologiaMovil/src/audinsa/audiologia/CuestionarioResultadoActivity.java
@@ -11,43 +11,42 @@ public class CuestionarioResultadoActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		//pantalla vertical
+		// pantalla vertical
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cuestionario_resultado);
-		
-		TextView txtResultadoDescription = (TextView)findViewById(R.id.txtResultadoDescription);
-		String strResultado=getIntent().getStringExtra("strResultado");
+
+		TextView txtResultadoDescription = (TextView) findViewById(R.id.txtResultadoDescription);
+		String strResultado = getIntent().getStringExtra("strResultado");
 		txtResultadoDescription.setText(strResultado);
 		// Cambia la imagen del semaforo si esta aprobado o reprobado
-		boolean aprobado =  getIntent().getBooleanExtra("bolAprobado", true);
-		ImageView img = (ImageView)findViewById(R.id.imgViewCuestionarioAprobado);
-		if (aprobado)
-		{
+		boolean aprobado = getIntent().getBooleanExtra("bolAprobado", true);
+		ImageView img = (ImageView) findViewById(R.id.imgViewCuestionarioAprobado);
+		if (aprobado) {
 			img.setImageResource(R.drawable.animation_resultado_aprobado);
-		}
-		else
-		{
+		} else {
 			img.setImageResource(R.drawable.animation_resultado_reprobado);
 		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present(pantalla horizontal).
-		
-		getMenuInflater().inflate(R.menu.activity_cuestionario_resultado, menu);		
+		// Inflate the menu; this adds items to the action bar if it is
+		// present(pantalla horizontal).
+
+		getMenuInflater().inflate(R.menu.activity_cuestionario_resultado, menu);
 		@SuppressWarnings("unused")
-		TextView txtResultadoDescription = (TextView)findViewById(R.id.txtResultadoDescription);
-				
+		TextView txtResultadoDescription = (TextView) findViewById(R.id.txtResultadoDescription);
+
 		return true;
 	}
-	
+
 	public void onWindowFocusChanged(boolean hasFocus) {
 
 		super.onWindowFocusChanged(hasFocus);
 
-		ImageView img = (ImageView)findViewById(R.id.imgViewCuestionarioAprobado);
-		AnimationDrawable frameAnimation = (AnimationDrawable)img.getDrawable();
+		ImageView img = (ImageView) findViewById(R.id.imgViewCuestionarioAprobado);
+		AnimationDrawable frameAnimation = (AnimationDrawable) img
+				.getDrawable();
 		frameAnimation.setCallback(img);
 		frameAnimation.setVisible(true, true);
 		frameAnimation.start();
