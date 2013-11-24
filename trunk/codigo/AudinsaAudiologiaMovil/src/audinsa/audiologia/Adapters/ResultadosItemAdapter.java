1c0dcc0c7f4a97f0be7209410b0ac3e5e66dfc1b
daniulate@gmail.com <daniulate@gmail.com@3e832744-ce6b-47cc-aa80-82fe731f6e6d>
2013-11-24 22:26:54 +0000

    despliegue de resultados

--- trunk/codigo/AudinsaAudiologiaMovil/src/audinsa/audiologia/Adapters/ResultadosItemAdapter.java
+++ trunk/codigo/AudinsaAudiologiaMovil/src/audinsa/audiologia/Adapters/ResultadosItemAdapter.java
@@ -10,6 +10,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import audinsa.audiologia.R;
 import audinsa.audiologia.businessdomain.Resultado;
+import audinsa.audiologia.businessdomain.TipoExamen;
+import audinsa.audiologia.datasources.ExamenDataSource;
+import audinsa.audiologia.datasources.TipoExamenDataSource;
 
 public class ResultadosItemAdapter extends ArrayAdapter<Resultado> {
 
@@ -52,17 +55,24 @@ public class ResultadosItemAdapter extends ArrayAdapter<Resultado> {
 		public View getView(int position, View convertView, ViewGroup parent) {
 			View row = convertView;
 			ResultadoHolder holder = null;
-
+			int idtipoExamen=0;
+			int idExamen=0;
+			String nomExamen;
+			ExamenDataSource datasourceExamen = new ExamenDataSource(context);
+			TipoExamenDataSource datasourceTipoExamen = new TipoExamenDataSource(context);
+			
+			
 			if(row == null)
 			{
 				LayoutInflater inflater = ((Activity)context).getLayoutInflater();
 				row = inflater.inflate(layoutResourceId, parent, false);
 
 				holder = new ResultadoHolder();
-				//holder.txtValorResultado = (TextView)row.findViewById(R.id.txtValorResultado);
+				holder.txtValorExamen = (TextView)row.findViewById(R.id.txtValorExamen);
 				holder.txtNombreExamen = (TextView)row.findViewById(R.id.txtNombreExamen);
-		    	//holder.txtDescripcionExamen = (TextView)row.findViewById(R.id.txtDescripcionExamen);
-				row.setTag(holder);
+			    holder.imgTipoExamen = (ImageView)row.findViewById(R.id.imgTipoExamen);
+				row.setTag(holder);			
+			
 			}
 			else
 			{
@@ -70,16 +80,47 @@ public class ResultadosItemAdapter extends ArrayAdapter<Resultado> {
 			}
 
 			Resultado resultado = getItem(position);
-			holder.txtNombreExamen.setText(resultado.getValor_examen());
+			try{
+				
+				holder.txtValorExamen.setText(Integer.toString(resultado.getValor_examen()));
+			   //Permite obtener los valores para ir a obtener el nombre del
+				//Tipo de exam.
+				idExamen=resultado.getId_examen();
+			    idtipoExamen= (datasourceExamen.buscarExamen(idExamen)).getId_tipo_examen();//obtiene el idtipoexamen del exam del resultado
+			    nomExamen= (datasourceTipoExamen.buscarTipoExamen(idtipoExamen)).getNombreExamen();
+			    holder.txtNombreExamen.setText(nomExamen);
+			     
+			    switch (idtipoExamen){
+			    case 1:
+			    	 holder.imgTipoExamen.setImageResource(R.drawable.ic_hearing_sensibility);
+					   break;
+			    case 2:
+			    	 holder.imgTipoExamen.setImageResource(R.drawable.ic_hablaenruido);
+					   break;
+			    case 3:
+			    	 holder.imgTipoExamen.setImageResource(R.drawable.ic_test);
+					   break;
+			    default:
+			    	break;
+			    
+			    }
+			    
+			  
+			}
+			catch(Exception e ){
+				e.getMessage();
+				
+			}		
+			
 
 			return row;
 		}
 		
 		static class ResultadoHolder
 		{
-			//TextView txtValorResultado;
+			TextView txtValorExamen;
 			TextView txtNombreExamen;			
-			//ImageView txtDescripcionExamen;
+			ImageView imgTipoExamen;
 			
 			
 		}
