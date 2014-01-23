51ffb15f7eb3b606db825e0cec1c4c4fa9005fca
daniulate <daniulate@gmail.com>
2014-01-23 03:40:51 +0000

    contiene contactar en ResultadoPerfilActivity.java y en CuestionarioResultadoActivity.java

--- codigo/AudinsaAudiologiaMovil/src/audinsa/audiologia/ResultadoPerfilActivity.java
+++ codigo/AudinsaAudiologiaMovil/src/audinsa/audiologia/ResultadoPerfilActivity.java
@@ -156,7 +156,7 @@ public class ResultadoPerfilActivity extends Activity {
 		contactIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
 		String[] mail = { "info@clinicaaudinsa.com", "" };
 		contactIntent.putExtra(Intent.EXTRA_EMAIL, mail);   
-        startActivity(Intent.createChooser(contactIntent, "Compartir usando"));
+        startActivity(Intent.createChooser(contactIntent, "Enviar información usando"));
 
 		
 	}
